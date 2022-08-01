272df744b17ec9c941c65f1b455725f552e5d88e
Simone <sbaglivi@gmail.com>
2022-08-02 00:39:22 +0200

    some more styling on search form

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import styles from './SearchForm.module.css';
+import image from '../assets/cocktail.png';
+import searchIcon from '../assets/search.svg';
 
 const SearchForm = ({handleSubmit}) => {
     const [text,setText] = useState('');
@@ -8,9 +10,17 @@ const SearchForm = ({handleSubmit}) => {
         handleSubmit(text);
     }
     return (
-        <form onSubmit={onSubmit}>
-            <input className={styles.textInput} type='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='Search for a cocktail'/>
-            <button>Search</button>
+        <form onSubmit={onSubmit} className={styles.searchForm}>
+            <div className={styles.imageDiv}>
+                <img src={image} alt='cocktail'/>
+            </div>
+            <div>
+                <h1 className={styles.welcome}>Welcome, what may I serve you?</h1>
+                <div className={styles.inputDiv}>
+                    <input className={styles.textInput} type='text' value={text} onChange={(e)=>setText(e.target.value)} placeholder='Search for a cocktail'/>
+                    <button className={styles.searchButton}><img src={searchIcon} alt='search'/></button>
+                </div>
+            </div>
         </form>
     )
 }
